23f6dbd3a1533ab641bddf8c0f4f95e510e999a3
sandipa pande <sandipa.pande@selu.edu>
2020-08-25 13:17:44 -0500

    My Practise Files
    Bootcamp Code Practise

---
+++ script.js
@@ -0,0 +1,11 @@
+var age = prompt("enter your age");
+
+if (age < 0){
+	console.log("age is negative");
+}
+if (age === 21){
+	console.log("happy 21st birthday")
+}
+if(age % 2 == 1 ){
+	console.log("age is odd");
+}
